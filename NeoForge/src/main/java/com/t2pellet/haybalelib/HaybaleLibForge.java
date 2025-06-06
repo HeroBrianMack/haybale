d14acdeea7808baa3e5d55a5713388cea5f7a9d2
Hero <herostillplays@gmail.com>
2025-06-06 15:09:54 -0400

    Add NeoForge

---
+++ NeoForge/src/main/java/com/t2pellet/haybalelib/HaybaleLibForge.java
@@ -0,0 +1,51 @@
+package com.t2pellet.haybalelib;
+
+import com.t2pellet.haybalelib.client.HaybaleLibModClient;
+import com.t2pellet.haybalelib.client.HaybaleLibClient;
+import com.t2pellet.haybalelib.services.ForgeSidedExecutor;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Mod(HaybaleLib.MODID)
+@HaybaleLibMod.IMod(HaybaleLib.MODID)
+public class HaybaleLibForge extends HaybaleLibForgeMod {
+
+    private static HaybaleLibForge instance = null;
+    public static HaybaleLibForge getInstance() {
+        return instance;
+    }
+
+    private Map<String, HaybaleLibForgeMod> modMap;
+
+    @Override
+    protected void initialSetup() {
+        instance = this;
+        modMap = new HashMap<>();
+    }
+
+    @Override
+    protected HaybaleLibMod getCommonMod() {
+        return HaybaleLib.INSTANCE;
+    }
+
+    @Override
+    protected HaybaleLibModClient getClientMod() {
+        return HaybaleLibClient.INSTANCE;
+    }
+
+    @Override
+    protected void registerEvents() {
+        MinecraftForge.EVENT_BUS.addListener(((ForgeSidedExecutor) Services.SIDE)::onServerTick);
+    }
+
+    public void register(String id, HaybaleLibForgeMod mod) {
+        modMap.put(id, mod);
+    }
+
+    public HaybaleLibForgeMod get(String modid) {
+        return modMap.get(modid);
+    }
+}
